0ef40a07b78b6e46fb078907e2a4959538197987
FickleBoBo <yukmw0704@gmail.com>
2026-01-09 05:18:37 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/solvemeup/smucoreapi/SmuCoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.solvemeup.smucoreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmuCoreApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmuCoreApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/solvemeup/smucoreapi/SmuCoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solvemeup.smucoreapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmuCoreApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

9ba942ba650eac798eb691196e5950f1a69a3350
FickleBoBo <yukmw0704@gmail.com>
2026-01-14 16:15:18 +0900

    feat: 커스텀 예외 작성

---
+++ src/main/java/com/solvemeup/smucoreapi/global/exception/UnauthorizedException.java
@@ -0,0 +1,7 @@
+package com.solvemeup.smucoreapi.global.exception;
+
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}

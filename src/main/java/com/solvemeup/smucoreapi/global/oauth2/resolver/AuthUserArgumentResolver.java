416141242e159cdcf961c16c17be372a92169fc2
FickleBoBo <yukmw0704@gmail.com>
2026-01-14 16:13:23 +0900

    feat: 인증 커스텀 어노테이션용 Argument Resolver 작성

---
+++ src/main/java/com/solvemeup/smucoreapi/global/oauth2/resolver/AuthUserArgumentResolver.java
@@ -0,0 +1,46 @@
+package com.solvemeup.smucoreapi.global.oauth2.resolver;
+
+import com.solvemeup.smucoreapi.global.exception.UnauthorizedException;
+import com.solvemeup.smucoreapi.global.oauth2.annotation.AuthUser;
+import com.solvemeup.smucoreapi.global.oauth2.session.UserSession;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.jspecify.annotations.Nullable;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+public class AuthUserArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(AuthUser.class) &&
+                parameter.getParameterType().equals(UserSession.class);
+    }
+
+    @Override
+    public @Nullable Object resolveArgument(MethodParameter parameter,
+                                            @Nullable ModelAndViewContainer mavContainer,
+                                            NativeWebRequest webRequest,
+                                            @Nullable WebDataBinderFactory binderFactory) {
+
+        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+        HttpSession session = request.getSession(false);
+
+        if (session == null) {
+            throw new UnauthorizedException("로그인이 필요합니다.");
+        }
+
+        UserSession userSession = (UserSession) session.getAttribute("USER_SESSION");
+
+        if (userSession == null) {
+            throw new UnauthorizedException("세션이 만료되었습니다.");
+        }
+
+        return userSession;
+    }
+}

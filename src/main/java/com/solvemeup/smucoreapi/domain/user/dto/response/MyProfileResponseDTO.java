dc7c9edbb248fc3ab555f86f75b327fcc5dc01ae
FickleBoBo <yukmw0704@gmail.com>
2026-01-14 16:15:05 +0900

    feat: 개인 정보 응답 DTO 작성

---
+++ src/main/java/com/solvemeup/smucoreapi/domain/user/dto/response/MyProfileResponseDTO.java
@@ -0,0 +1,34 @@
+package com.solvemeup.smucoreapi.domain.user.dto.response;
+
+import com.solvemeup.smucoreapi.domain.user.entity.User;
+import com.solvemeup.smucoreapi.domain.user.enums.Role;
+import com.solvemeup.smucoreapi.domain.user.enums.Status;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class MyProfileResponseDTO {
+
+    private String email;
+    private String nickname;
+    private String profileImageUrl;
+    private String githubUrl;
+    private String techblogUrl;
+    private int rating;
+    private Role role;
+    private Status status;
+
+    public static MyProfileResponseDTO from(User user) {
+        return MyProfileResponseDTO.builder()
+                .email(user.getEmail())
+                .nickname(user.getNickname())
+                .profileImageUrl(user.getProfileImageUrl())
+                .githubUrl(user.getGithubUrl())
+                .techblogUrl(user.getTechblogUrl())
+                .rating(user.getRating())
+                .role(user.getRole())
+                .status(user.getStatus())
+                .build();
+    }
+}

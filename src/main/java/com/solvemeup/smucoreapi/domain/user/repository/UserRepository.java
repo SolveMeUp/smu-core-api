b7720438618ae533ef8780fad1826e2933b00485
FickleBoBo <yukmw0704@gmail.com>
2026-01-16 09:48:28 +0900

    feat: 모든 유저 랭킹 조회, 특정 유저 조회, 유저 본인 조회 API 작성

--- src/main/java/com/solvemeup/smucoreapi/domain/user/repository/UserRepository.java
+++ src/main/java/com/solvemeup/smucoreapi/domain/user/repository/UserRepository.java
@@ -2,15 +2,49 @@ package com.solvemeup.smucoreapi.domain.user.repository;
 
 import com.solvemeup.smucoreapi.domain.user.entity.User;
 import com.solvemeup.smucoreapi.domain.user.enums.OAuth2Provider;
+import com.solvemeup.smucoreapi.domain.user.repository.projection.UserRankingProjection;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByOauth2ProviderAndOauth2ProviderId(OAuth2Provider oauth2Provider, String oauth2ProviderId);
 
     Optional<User> findByNickname(String nickname);
+
+    @Query(
+            value = """
+                    SELECT
+                        u.nickname AS nickname,
+                        u.profile_image_url AS profileImageUrl,
+                        u.github_url AS githubUrl,
+                        u.techblog_url AS techblogUrl,
+                        u.rating AS rating,
+                        u.role AS role,
+                        u.status AS status,
+                        RANK() OVER (ORDER BY u.rating DESC) AS `rank`
+                    FROM users u
+                    WHERE u.status != 'DELETED'
+                    ORDER BY u.rating DESC, u.id
+                    """,
+            countQuery = """
+                    SELECT COUNT(*)
+                    FROM users u
+                    WHERE u.status != 'DELETED'
+                    """,
+            nativeQuery = true
+    )
+    Page<UserRankingProjection> findRankingWithRank(Pageable pageable);
+
+    @Query(value = """
+            SELECT COUNT(*) + 1
+            FROM users u
+            WHERE u.status != 'DELETED' AND u.rating > :rating
+            """, nativeQuery = true)
+    long calcCompetitionRankByRating(@Param("rating") int rating);
 }

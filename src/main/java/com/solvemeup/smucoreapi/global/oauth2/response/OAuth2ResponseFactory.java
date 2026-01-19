adfa1ed2eb42cc525207faaf2256f6b93d17b1ee
FickleBoBo <yukmw0704@gmail.com>
2026-01-19 21:34:15 +0900

    feat: OAuth2Response 팩터리 클래스 작성

---
+++ src/main/java/com/solvemeup/smucoreapi/global/oauth2/response/OAuth2ResponseFactory.java
@@ -0,0 +1,31 @@
+package com.solvemeup.smucoreapi.global.oauth2.response;
+
+import com.solvemeup.smucoreapi.domain.user.enums.OAuth2Provider;
+import com.solvemeup.smucoreapi.global.oauth2.exception.InvalidOAuth2RegistrationIdException;
+import com.solvemeup.smucoreapi.global.oauth2.exception.UnsupportedOAuth2ProviderException;
+
+import java.util.Map;
+
+public final class OAuth2ResponseFactory {
+
+    private OAuth2ResponseFactory() {
+        throw new AssertionError("OAuth2ResponseFactory is a Utility class");
+    }
+
+    public static OAuth2Response of(String registrationId, Map<String, Object> attributes) {
+        OAuth2Provider provider = parseProvider(registrationId);
+
+        return switch (provider) {
+            case GITHUB -> new GithubResponse(attributes);
+            default -> throw new UnsupportedOAuth2ProviderException(registrationId);
+        };
+    }
+
+    private static OAuth2Provider parseProvider(String registrationId) {
+        try {
+            return OAuth2Provider.valueOf(registrationId.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            throw new InvalidOAuth2RegistrationIdException(registrationId);
+        }
+    }
+}

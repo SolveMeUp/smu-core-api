c376b90108ca16b119dd723c31db78feb7d27c1c
FickleBoBo <yukmw0704@gmail.com>
2026-01-19 21:33:55 +0900

    feat: OAuth2Response 상속 방식으로 구조 변경

---
+++ src/main/java/com/solvemeup/smucoreapi/global/oauth2/response/GithubResponse.java
@@ -0,0 +1,40 @@
+package com.solvemeup.smucoreapi.global.oauth2.response;
+
+import com.solvemeup.smucoreapi.domain.user.enums.OAuth2Provider;
+import com.solvemeup.smucoreapi.global.oauth2.exception.OAuth2AttributeMissingException;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+import java.util.Map;
+
+import static com.solvemeup.smucoreapi.domain.user.enums.OAuth2Provider.GITHUB;
+
+@RequiredArgsConstructor
+@ToString
+public final class GithubResponse implements OAuth2Response {
+
+    private final Map<String, Object> attributes;
+
+    @Override
+    public OAuth2Provider getOAuth2Provider() {
+        return GITHUB;
+    }
+
+    @Override
+    public String getOAuth2ProviderId() {
+        return requiredString("id");
+    }
+
+    @Override
+    public String getProfileImageUrl() {
+        return requiredString("avatar_url");
+    }
+
+    private String requiredString(String key) {
+        Object value = attributes.get(key);
+        if (value == null) {
+            throw new OAuth2AttributeMissingException(GITHUB, key);
+        }
+        return value.toString();
+    }
+}

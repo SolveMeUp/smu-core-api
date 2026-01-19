7b170bcb49d6735fc2909e83c589fb514da23c18
FickleBoBo <yukmw0704@gmail.com>
2026-01-19 22:01:47 +0900

    feat: User 도메인 에러코드 추가

--- src/main/java/com/solvemeup/smucoreapi/global/exception/ErrorCode.java
+++ src/main/java/com/solvemeup/smucoreapi/global/exception/ErrorCode.java
@@ -15,6 +15,10 @@ public enum ErrorCode {
     OAUTH2_INVALID_REGISTRATION_ID(HttpStatus.BAD_REQUEST, "OAUTH2-002", "잘못된 OAuth2 registrationId 입니다."),
     OAUTH2_MISSING_ATTRIBUTE(HttpStatus.INTERNAL_SERVER_ERROR, "OAUTH2-003", "OAuth2 응답 필수 값이 누락되었습니다."),
 
+    // User
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "USER-001", "유저를 찾을 수 없습니다."),
+    NICKNAME_GENERATION_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "USER-002", "닉네임 생성에 실패했습니다."),
+
     // Common
     COMMON_BAD_REQUEST(HttpStatus.BAD_REQUEST, "COMMON-001", "잘못된 요청입니다."),
     COMMON_INTERNAL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON-002", "서버 오류가 발생했습니다.");

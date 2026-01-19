55732595d0037cfcbb142dd5d4e2def4a9eb3c3c
FickleBoBo <yukmw0704@gmail.com>
2026-01-19 21:30:33 +0900

    refactor: AuthUser 커스텀 어노테이션 CustomOAuth2User 기반으로 변경

---
+++ src/main/java/com/solvemeup/smucoreapi/global/auth/AuthUser.java
@@ -0,0 +1,12 @@
+package com.solvemeup.smucoreapi.global.auth;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@AuthenticationPrincipal
+public @interface AuthUser {
+}

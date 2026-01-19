77eb76481fc59705e155cd7287e4ee086accd7da
FickleBoBo <yukmw0704@gmail.com>
2026-01-19 22:02:16 +0900

    refactor: 본인 정보 응답 DTO 필드 추가

--- src/main/java/com/solvemeup/smucoreapi/domain/user/dto/response/MyProfileResponseDTO.java
+++ src/main/java/com/solvemeup/smucoreapi/domain/user/dto/response/MyProfileResponseDTO.java
@@ -1,10 +1,15 @@
 package com.solvemeup.smucoreapi.domain.user.dto.response;
 
 import com.solvemeup.smucoreapi.domain.user.entity.User;
+import com.solvemeup.smucoreapi.domain.user.enums.OAuth2Provider;
 import com.solvemeup.smucoreapi.domain.user.enums.Role;
 import com.solvemeup.smucoreapi.domain.user.enums.Status;
 
+import java.time.Instant;
+
 public record MyProfileResponseDTO(
+        Long id,
+        OAuth2Provider oAuth2Provider,
         String email,
         String nickname,
         String profileImageUrl,
@@ -12,11 +17,14 @@ public record MyProfileResponseDTO(
         String techblogUrl,
         int rating,
         Role role,
+        Instant createdAt,
         Status status,
         long rank
 ) {
     public static MyProfileResponseDTO from(User user, long rank) {
         return new MyProfileResponseDTO(
+                user.getId(),
+                user.getOauth2Provider(),
                 user.getEmail(),
                 user.getNickname(),
                 user.getProfileImageUrl(),
@@ -24,6 +32,7 @@ public record MyProfileResponseDTO(
                 user.getTechblogUrl(),
                 user.getRating(),
                 user.getRole(),
+                user.getCreatedAt(),
                 user.getStatus(),
                 rank
         );

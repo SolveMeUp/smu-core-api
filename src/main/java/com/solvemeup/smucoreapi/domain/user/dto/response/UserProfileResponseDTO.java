11ce383df9f521b09976808251e7142a69284737
FickleBoBo <yukmw0704@gmail.com>
2026-01-20 17:11:11 +0900

    refactor: 특정 유저 조회 API 반환 필드 추가

--- src/main/java/com/solvemeup/smucoreapi/domain/user/dto/response/UserProfileResponseDTO.java
+++ src/main/java/com/solvemeup/smucoreapi/domain/user/dto/response/UserProfileResponseDTO.java
@@ -1,11 +1,14 @@
 package com.solvemeup.smucoreapi.domain.user.dto.response;
 
 import com.solvemeup.smucoreapi.domain.user.entity.User;
+import com.solvemeup.smucoreapi.domain.user.enums.OAuth2Provider;
 import com.solvemeup.smucoreapi.domain.user.enums.Role;
 import com.solvemeup.smucoreapi.domain.user.enums.Status;
 import com.solvemeup.smucoreapi.domain.user.repository.projection.UserRankingProjection;
 
 public record UserProfileResponseDTO(
+        Long id,
+        OAuth2Provider oAuth2Provider,
         String nickname,
         String profileImageUrl,
         String githubUrl,
@@ -17,6 +20,8 @@ public record UserProfileResponseDTO(
 ) {
     public static UserProfileResponseDTO from(User user, long rank) {
         return new UserProfileResponseDTO(
+                user.getId(),
+                user.getOauth2Provider(),
                 user.getNickname(),
                 user.getProfileImageUrl(),
                 user.getGithubUrl(),
@@ -30,6 +35,8 @@ public record UserProfileResponseDTO(
 
     public static UserProfileResponseDTO from(UserRankingProjection p) {
         return new UserProfileResponseDTO(
+                p.getId(),
+                p.getOAuth2Provider(),
                 p.getNickname(),
                 p.getProfileImageUrl(),
                 p.getGithubUrl(),
--- src/main/java/com/solvemeup/smucoreapi/domain/user/repository/projection/UserRankingProjection.java
+++ src/main/java/com/solvemeup/smucoreapi/domain/user/repository/projection/UserRankingProjection.java
@@ -1,6 +1,13 @@
 package com.solvemeup.smucoreapi.domain.user.repository.projection;
 
+import com.solvemeup.smucoreapi.domain.user.enums.OAuth2Provider;
+
 public interface UserRankingProjection {
+
+    Long getId();
+
+    OAuth2Provider getOAuth2Provider();
+
     String getNickname();
 
     String getProfileImageUrl();

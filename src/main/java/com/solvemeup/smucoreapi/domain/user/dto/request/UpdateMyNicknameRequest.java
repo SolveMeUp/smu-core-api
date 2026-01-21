0f97f6d59bfa3dd1bf656b72d19407a25a01bf09
FickleBoBo <yukmw0704@gmail.com>
2026-01-21 20:37:23 +0900

    rename: dto 클래스명 변경

--- src/main/java/com/solvemeup/smucoreapi/domain/user/dto/request/UpdateMyNicknameRequest.java
+++ src/main/java/com/solvemeup/smucoreapi/domain/user/dto/request/UpdateMyNicknameRequest.java
@@ -3,7 +3,7 @@ package com.solvemeup.smucoreapi.domain.user.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-public record UpdateMyNicknameRequestDTO(
+public record UpdateMyNicknameRequest(
         @NotBlank(message = "닉네임은 필수입니다.")
         @Size(min = 4, max = 20, message = "닉네임은 최소 4자에서 최대 20자까지 가능합니다.")
         String nickname

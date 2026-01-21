da3d978c77261d0a41fc3acb069da379c40ef4f4
FickleBoBo <yukmw0704@gmail.com>
2026-01-21 20:39:22 +0900

    refactor: 생성자 변경

--- src/main/java/com/solvemeup/smucoreapi/domain/user/exception/NicknameAlreadyExistsException.java
+++ src/main/java/com/solvemeup/smucoreapi/domain/user/exception/NicknameAlreadyExistsException.java
@@ -6,7 +6,7 @@ import static com.solvemeup.smucoreapi.global.exception.ErrorCode.*;
 
 public class NicknameAlreadyExistsException extends CustomException {
 
-    public NicknameAlreadyExistsException() {
-        super(NICKNAME_ALREADY_EXISTS);
+    public NicknameAlreadyExistsException(String nickname) {
+        super(NICKNAME_ALREADY_EXISTS, nickname + " already exists!");
     }
 }

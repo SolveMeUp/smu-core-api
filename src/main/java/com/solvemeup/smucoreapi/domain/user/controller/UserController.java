4d5ec981510d6cb14b198f97d388e36bc0fb70ff
FickleBoBo <yukmw0704@gmail.com>
2026-02-03 16:25:32 +0900

    chore: 코드 스타일 변경

--- src/main/java/com/solvemeup/smucoreapi/domain/user/controller/UserController.java
+++ src/main/java/com/solvemeup/smucoreapi/domain/user/controller/UserController.java
@@ -91,8 +91,9 @@ public class UserController {
     }
 
     @GetMapping("/ranking")
-    public ResponseEntity<PageResponse<UserProfileResponse>> getRanking(@RequestParam(defaultValue = "0") @Min(0) @Max(100000) int page,
-                                                                        @RequestParam(defaultValue = "100") @Min(10) @Max(100) int size
+    public ResponseEntity<PageResponse<UserProfileResponse>> getRanking(
+            @RequestParam(defaultValue = "0") @Min(0) @Max(100000) int page,
+            @RequestParam(defaultValue = "100") @Min(10) @Max(100) int size
     ) {
         Pageable pageable = PageRequest.of(page, size);
         return ResponseEntity.ok(PageResponse.from(userService.getRanking(pageable)));

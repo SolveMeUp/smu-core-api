9c5fde2a51222a7f5e917620e88321f1b7e238cb
FickleBoBo <yukmw0704@gmail.com>
2026-02-03 16:27:37 +0900

    refactor: 생성자 시그니처 수정

--- src/main/java/com/solvemeup/smucoreapi/domain/user/exception/NicknameAlreadyExistsException.java
+++ src/main/java/com/solvemeup/smucoreapi/domain/user/exception/NicknameAlreadyExistsException.java
@@ -9,7 +9,7 @@ import static com.solvemeup.smucoreapi.global.exception.ErrorCode.*;
  */
 public class NicknameAlreadyExistsException extends CustomException {
 
-    public NicknameAlreadyExistsException(String nickname) {
-        super(NICKNAME_ALREADY_EXISTS, nickname + " already exists!");
+    public NicknameAlreadyExistsException() {
+        super(NICKNAME_ALREADY_EXISTS);
     }
 }
--- src/main/java/com/solvemeup/smucoreapi/domain/user/exception/UserNotFoundException.java
+++ src/main/java/com/solvemeup/smucoreapi/domain/user/exception/UserNotFoundException.java
@@ -9,7 +9,7 @@ import static com.solvemeup.smucoreapi.global.exception.ErrorCode.*;
  */
 public class UserNotFoundException extends CustomException {
 
-    public UserNotFoundException(Long userId) {
-        super(USER_NOT_FOUND, "User not found: userId=" + userId);
+    public UserNotFoundException() {
+        super(USER_NOT_FOUND);
     }
 }

36ecd6b86ec14bcd689802bc0bc0928234bd5f46
FickleBoBo <yukmw0704@gmail.com>
2026-06-06 02:43:45 +0900

    feat: 프로필 URL 길이 검증 추가
    github/techblog URL 요청 DTO에 @Size(max=2083) 추가하여
    엔티티 컬럼 길이와 정합.

--- src/main/java/com/solvemeup/smucoreapi/domain/user/dto/request/UpdateMyTechblogUrlRequest.java
+++ src/main/java/com/solvemeup/smucoreapi/domain/user/dto/request/UpdateMyTechblogUrlRequest.java
@@ -1,10 +1,12 @@
 package com.solvemeup.smucoreapi.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import org.hibernate.validator.constraints.URL;
 
 public record UpdateMyTechblogUrlRequest(
         @NotBlank(message = "url은 필수입니다.")
+        @Size(max = 2083, message = "url은 2083자를 초과할 수 없습니다.")
         @URL(message = "올바른 URL 형식이 아닙니다.")
         String techblogUrl
 ) {
